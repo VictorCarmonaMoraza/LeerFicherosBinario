dfaca0bf5f893354b5969d842f587c682602c531
Victor Carmona <VictorCarmona9035@hotmail.com>
2022-01-27 08:13:46 +0100

    1-Creacion del proyecto

---
+++ LeerArchivoBinario_Net_Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeerArchivoBinario_Net_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
